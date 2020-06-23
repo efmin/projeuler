f0504c1657231357984ecedde4811ff1c8847435
EFFY MIN <effyverse@gmail.com>
2020-06-23 10:10:21 -0400

    detect_paragram

---
+++ ruby/detect_panagram.rb
@@ -0,0 +1,31 @@
+A pangram is a sentence that contains every single letter of the alphabet
+at least once. For example, the sentence
+"The quick brown fox jumps over the lazy dog" is a pangram
+because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram.
+Return True if it is, False if not. Ignore numbers and punctuation.
+
+10DIME
+
+Input
+string
+
+Output
+boolean
+
+Definitions/Data Structures
+panagram -> sentance containing every single letter of alphabet at least once
+(could be more than once)
+
+try to keep as string?
+
+Illustrations
+I need a method that takes a string as input and checks if it includes
+each letter of the alphabet. Return true is so, false if not.
+
+Methods
+include?
+
+Edge Cases
+if it contains extra letters, that's fine
